0b78c3e259cb747749c71183e4dc3b3ae0429309
Agbe Terseer <agbe.terseer@gmail.com>
2019-11-05 15:28:36 +0100

    Entry pint

--- src/main/java/com/fsi_crud_api/FsiCrudApiApplication.java
+++ src/main/java/com/fsi_crud_api/FsiCrudApiApplication.java
@@ -1,18 +1,13 @@
 package com.fsi_crud_api;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FsiCrudApiApplication {
-    
-    private static final Logger logger = LoggerFactory.getLogger(FsiCrudApiApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(FsiCrudApiApplication.class, args);
-	}
-        
- 
+    public static void main(String[] args) {
+        SpringApplication.run(FsiCrudApiApplication.class, args);
+    }
+
 }
